735da245d8df342e833d64d6d4fd9412eadad501
Geoyeob Kim <gasbank@msn.com>
2018-03-23 03:16:12 +0900

    simpler approach

--- precompiled.hpp
+++ precompiled.hpp
@@ -11,5 +11,12 @@
 #include <limits>
 #include <list>
 #include <set>
+#include <unordered_set>
+#include <boost/interprocess/managed_mapped_file.hpp>
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point.hpp>
+#include <boost/geometry/geometries/box.hpp>
+#include <boost/geometry/index/rtree.hpp>
+
 #define PNG_DEBUG 3
 #include <png.h>
